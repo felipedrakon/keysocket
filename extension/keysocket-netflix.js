42460af795d94b452fe67a6c1fbdeffd357aaeea
felipedrakon <37990496+felipedrakon@users.noreply.github.com>
2018-04-05 05:20:06 -0300

    Update keysocket-netflix.js
    Corrigido, testado e funcionando :D

--- extension/keysocket-netflix.js
+++ extension/keysocket-netflix.js
@@ -1,25 +1,22 @@
 keySocket.init(
     "nexflix",
     {
-        "play-pause": ".player-play-pause",
-        "prev": function () {
-            var prevButtonEpisode = null;
-            var episodeList = document.querySelector('li.episode-list-item--expanded');
-            if (episodeList) {
-                prevButtonEpisode = episodeList.previousElementSibling.querySelector('div.play-icon');
-                if (prevButtonEpisode) {
-                    episodeList.previousElementSibling.className += ' episode-list-item--expanded';
-                    keySocket.simulateClick(prevButtonEpisode);
-                }
-            }
-        },
-        "next": "div.player-next-episode",
-        "stop": function () {
-            var isPlaying = document.querySelector('div.player-play-pause.pause');
-            if (isPlaying) {
-                var playPauseButton = document.querySelector('.player-play-pause');
+        "play-pause": function () {
+                var playPauseButton = document.querySelector('.button-nfplayerPlay, .button-nfplayerPause');
                 keySocket.simulateClick(playPauseButton);
-            }
+        },
+        "prev": function () {   
+                var prevButton = document.querySelector('.prev');//Don't Find
+                keySocket.simulateClick(prevButton); 
+        },
+        "next":  function () {      
+                var nextButton = document.querySelector('.button-nfplayerNextEpisode');
+                keySocket.simulateClick(nextButton);     
+        },
+        "stop": function () {
+           
+                var stopButton = document.querySelector('.button-nfplayerPause');
+                keySocket.simulateClick(stopButton);   
         }
     }
 );
