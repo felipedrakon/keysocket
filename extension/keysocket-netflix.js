1e379e84c4c001615ce4a06c56418ecaabfdd0a1
Felipe DraKon <37990496+felipedrakon@users.noreply.github.com>
2018-04-07 03:22:42 -0300

    Update keysocket-netflix.js
    Amigo da forma que eu enviei está funcionando!

--- extension/keysocket-netflix.js
+++ extension/keysocket-netflix.js
@@ -1,6 +1,6 @@
 function onKeyPress(key) {
     if (key === NEXT) {
-        var nextButtonEpisode = document.querySelector('div.player-next-episode');
+        var nextButtonEpisode = document.querySelector('.button-nfplayerNextEpisode');
         simulateClick(nextButtonEpisode);
     } else if (key === PREV) {
         var prevButtonEpisode = null;
@@ -13,14 +13,11 @@ function onKeyPress(key) {
             }
         }
     } else if (key === PLAY) {
-        playPauseButton = document.querySelector('.player-play-pause');
+        playPauseButton = document.querySelector('.button-nfplayerPlay, .button-nfplayerPause');
         simulateClick(playPauseButton);
     } else if (key === STOP) {
-        var isPlaying = document.querySelector('div.player-play-pause.pause');
-        if (isPlaying) {
-            var playPauseButton = document.querySelector('.player-play-pause');
+            var playPauseButton = document.querySelector('.button-nfplayerPause');
             simulateClick(playPauseButton);
-        }
     }
 }
 
